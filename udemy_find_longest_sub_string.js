da63f7f06c6027dcef1e5e0ddd65f2db0247da2e
Crew Spence <csspence123@gmail.com>
2020-06-07 05:00:50 -0500

    Created function stub for toy problem.

---
+++ udemy_find_longest_sub_string.js
@@ -0,0 +1,24 @@
+/*
+Udemy JavaScript Algorithms and Data Structures Masterclass
+
+Sliding Window - findLongestSubstring
+
+Write a function called findLongestSubstring, which accepts a string
+and returns the length of the longest substring with all distinct characters.
+
+Examples:
+findLongestSubstring(''); // 0
+findLongestSubstring('rithmschool'); // 7
+findLongestSubstring('thisisawesome'); // 6
+findLongestSubstring('thecatinthehat'); // 7
+findLongestSubstring('bbbbb'); // 1
+findLongestSubstring('longestsubstring'); // 8
+findLongestSubstring('thisishowwedoit'); // 6
+
+Time Complexity - O(n)
+
+*/
+
+const findLongestSubstring = (str) => {
+  
+}
